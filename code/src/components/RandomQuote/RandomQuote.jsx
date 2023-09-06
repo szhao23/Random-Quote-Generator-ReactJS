5bd5271635a2a8ca3ef5126d680201909467e1ca
szhao23 <stevenzhao456@gmai.com>
2023-09-06 16:57:39 -0500

    Modified Icons, Changed CSS Styling, Added Functionality for Fetching Data and Refreshing Quotes when Refresh Icon is selected

--- code/src/components/RandomQuote/RandomQuote.jsx
+++ code/src/components/RandomQuote/RandomQuote.jsx
@@ -4,10 +4,26 @@ import Twitter_Icon from "../assets/twitter-x-logo.png";
 import Refresh_Icon from "../assets/refresh-icon.png";
 
 const RandomQuote = () => {
+  // Store Data in Variable from API
+  let quotes = [];
+
+  async function loadQuotes() {
+    const response = await fetch("https://type.fit/api/quotes");
+    quotes = await response.json();
+  }
+
+  const random = () => {
+    const select = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(select);
+  };
+
   const [quote, setQuote] = useState({
     text: "Tough times build strong men.",
     author: "G. Michael Hopf.",
   });
+
+  loadQuotes();
+
   return (
     <div className="container">
       <div className="quote">{quote.text}</div>
@@ -16,7 +32,13 @@ const RandomQuote = () => {
         <div className="bottom">
           <div className="author">{quote.author}</div>
           <div className="icons">
-            <img src={Refresh_Icon} alt="" />
+            <img
+              src={Refresh_Icon}
+              onClick={() => {
+                random();
+              }}
+              alt=""
+            />
             <img src={Twitter_Icon} alt="" />
           </div>
         </div>
